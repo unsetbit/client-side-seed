2d37a009bb075908aa3f5b12ccd68b03f01539c6
Ozan Turgut <ozan@signalfuse.com>
2014-09-18 11:06:18 -0700

    Upgraded to boilerplate-gulp 0.3.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,7 @@ var gulp = require('gulp'),
   boilerplate = require('boilerplate-gulp');
 
 boilerplate(gulp, {
-  pkg: require('./package.json')
+  pkg: require('./package.json'),
+  jsMain: './src/js/seed.js',
+  cssMain: './src/css/seed.js'
 });
